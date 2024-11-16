967abf0324f20f7f9a1d91e33d1881d3b16ded87
gannont14 <tubbsgannon14@gmail.com>
2024-11-16 13:17:15 -0500

    updated data model

--- flaskr/schema.sql
+++ flaskr/schema.sql
@@ -15,3 +15,37 @@ CREATE TABLE post (
   body TEXT NOT NULL,
   FOREIGN KEY (author_id) REFERENCES user (id)
 );
+
+CREATE TABLE repositories (
+  repo_id INTEGER PRIMARY KEY AUTOINCREMENT,
+  FOREIGN KEY(product_id) REFERENCES products(product_id)
+  name TEXT NOT NULL,
+  url TEXT NOT NULL,
+  created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+);
+
+CREATE TABLE products (
+  poduct_id INTEGER PRIMARY KEY AUTOINCREMENT=10000,
+  name TEXT NOT NULL,
+  body TEXT NOT NULL,
+  created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+)
+
+CREATE TABLE contacts (
+  contact_id INTEGER PRIMARY KEY AUTOINCREMENT,
+  first_name TEXT NOT NULL,
+  last_name TEXT NOT NULL,
+  email TEXT NOT NULL,
+  chat_username TEXT,
+  location TEXT,
+  role TEXT NOT NULL,
+  created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+);
+
+CREATE TABLE product_contacts (
+  product_contact_id INTEGER PRIMARY KEY AUTOINCREMENT,
+  FOREIGN KEY(product_id) REFERENCES products(product_id)
+  FOREIGN KEY(contact_id) REFERENCES contacts(contact_id)
+  created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+);
+
