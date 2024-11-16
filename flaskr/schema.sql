5d011c84f38da662125f88aa19355e8be36a4a3a
gannont14 <tubbsgannon14@gmail.com>
2024-11-16 13:31:54 -0500

    updated schema

--- flaskr/schema.sql
+++ flaskr/schema.sql
@@ -1,5 +1,9 @@
 DROP TABLE IF EXISTS user;
 DROP TABLE IF EXISTS post;
+DROP TABLE IF EXISTS repositories;
+DROP TABLE IF EXISTS products;
+DROP TABLE IF EXISTS contacts;
+DROP TABLE IF EXISTS product_contacts;
 
 CREATE TABLE user (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -16,20 +20,21 @@ CREATE TABLE post (
   FOREIGN KEY (author_id) REFERENCES user (id)
 );
 
+CREATE TABLE products (
+  product_id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name TEXT NOT NULL,
+  body TEXT NOT NULL,
+  created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
 CREATE TABLE repositories (
   repo_id INTEGER PRIMARY KEY AUTOINCREMENT,
-  FOREIGN KEY(product_id) REFERENCES products(product_id)
+  product_id INTEGER NOT NULL,
   name TEXT NOT NULL,
   url TEXT NOT NULL,
   created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  FOREIGN KEY(product_id) REFERENCES products(product_id)
 );
-
-CREATE TABLE products (
-  poduct_id INTEGER PRIMARY KEY AUTOINCREMENT=10000,
-  name TEXT NOT NULL,
-  body TEXT NOT NULL,
-  created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-)
 
 CREATE TABLE contacts (
   contact_id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -39,13 +44,15 @@ CREATE TABLE contacts (
   chat_username TEXT,
   location TEXT,
   role TEXT NOT NULL,
-  created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE product_contacts (
   product_contact_id INTEGER PRIMARY KEY AUTOINCREMENT,
-  FOREIGN KEY(product_id) REFERENCES products(product_id)
-  FOREIGN KEY(contact_id) REFERENCES contacts(contact_id)
+  product_id INTEGER NOT NULL,
+  contact_id INTEGER NOT NULL,
   created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  FOREIGN KEY(product_id) REFERENCES products(product_id),
+  FOREIGN KEY(contact_id) REFERENCES contacts(contact_id)
 );
 
