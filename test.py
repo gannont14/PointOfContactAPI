5fd84dbfdb7bbb09f6e5b57ca28f7c48f13b1c78
MarioR17 <jmrendonq17@gmail.com>
2024-11-17 11:19:18 -0500

    removed one file and one directory

--- test.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-product_name = "Enterprise-wide scalable collaboration"
-requestURL = "http://127.0.0.1:5000/contact/products"
-PARAMS = {'product_name': product_name}
-
-data = requests.get(url=requestURL, params=PARAMS)
-print(data)
